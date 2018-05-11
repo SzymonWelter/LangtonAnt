bcfbf0972eb3de3963c7c60638f9b2911489d9bf
Szymziom <szymon.welter98@gmail.com>
2018-05-11 13:14:31 +0200

    powrot do mastera

---
+++ LangtonAntJavaFx/src/javaFiles/AntObserver.java
@@ -0,0 +1,21 @@
+package javaFiles;
+
+import controllers.AntsController;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class AntObserver implements Observer {
+
+    private AntsController antsController = AntsController.getInstance();
+
+
+    public AntObserver(Ant observableAnt){
+        antsController.showAntProperties(observableAnt);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        antsController.showAntProperties((Ant)o);
+    }
+}
